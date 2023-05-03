73e1451c87400c9a7b170927c6dc0cf4c2df858e
arkoaikins <arkoaikins@gmail.com>
2023-05-03 17:17:38 +0000

    prints single digits in base 10 using putchar

---
+++ Variables_if_else_while/numberswith_putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers using putchar
+ * Return: 0 if sucess
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
